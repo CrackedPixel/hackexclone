a51f7dae04c9349503870dc67e7e0b77995fbf2c
CrackedPixel <acarbajal721@live.com>
2019-11-15 07:01:33 -0700

    Added actions/reducers

--- front/hackexclone/src/Actions/actionCommands.js
+++ front/hackexclone/src/Actions/actionCommands.js
@@ -1,5 +1,6 @@
 import ma from './mainActions';
 
+// Setters 
 const SET_CHANGING_LOGIN = newVal => {
   return {
     type: ma.S_CHANGING_LOGIN,
@@ -14,4 +15,18 @@ const SET_GLOBAL_CLICK = newVal => {
   }
 }
 
-module.exports = {SET_CHANGING_LOGIN}
+const SET_DID_FADE_DASHBOARD = newVal => {
+  return {
+    type: ma.S_DID_FADE_DASHBOARD,
+    payload: newVal
+  }
+}
+
+const SET_USER_INFO = newVal => {
+  return {
+    type: ma.S_USER_INFO,
+    payload: newVal
+  }
+}
+
+module.exports = {SET_CHANGING_LOGIN, SET_GLOBAL_CLICK, SET_DID_FADE_DASHBOARD, SET_USER_INFO}
