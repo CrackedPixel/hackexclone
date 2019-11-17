06e6476cb08144e2c2b53e3647790c4b0092503d
CrackedPixel <acarbajal721@live.com>
2019-11-16 22:36:03 -0700

    Finished bank FE page 1

--- front/hackexclone/src/components/Apps/Bank.js
+++ front/hackexclone/src/components/Apps/Bank.js
@@ -1,18 +1,27 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom';
-
+import axiosWithAuth from '../../utils/axiosWithAuth';
 import { useSelector, useDispatch } from 'react-redux';
 
 // import CircularProgress from '@material-ui/core/CircularProgress';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 
 
 export const Bank = (props) => {
   const mainDispatch = useDispatch();
   const [errorMsg, setErrorMessage] = useState('');
+  const [isLoggedIntoBank, SILIB] = useState(false);
 
   const global_click = useSelector(state => state.GLOBAL_CLICK);
-  
+
+  if (localStorage.getItem("bank_proverbs")) {
+
+  }else{
+    // axiosWithAuth()
+    // .then()
+  }
+
   const handle_click = e => {
     if (!errorMsg && global_click){
       props.canClick();
@@ -24,7 +33,24 @@ export const Bank = (props) => {
 
   return (
     <div className="appPage bg-bank">
-
+    <section className="bank__top__section">
+      <div className="bank__logo__container">
+        <AccountBalanceIcon className="bank__logo" />
+        <span>pnt</span>
+      </div>
+      <div className="bank__subtitle__container">
+        <span>Welcome to PNT Bank</span>
+      </div>
+    </section>
+    <section className="bank__content__section">
+      <div className="bank__login__container">
+        <span>Login</span>
+        <input tabindex="-1" type="text" value="CrackedPixel" />
+        <input tabindex="-1" type="password" value="password" />
+        <button tabindex="1" >Login</button>
+      </div>
+      <span className="bank__content__proverb">"Inspire your inspirations"</span>
+    </section>
     <section className="bank__footer">
       <Link className="linkBtn" onClick={handle_click} to="/dashboard"><ExitToAppIcon className="back-icon flip-x"/><span>Back</span></Link>
     </section>
